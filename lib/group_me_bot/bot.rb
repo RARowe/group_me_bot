f5d21d158f082231e91d88fa3750e810e6daec87
Richard Rowe <richardrowe@ou.edu>
2015-02-17 16:49:59 -0600

    Send image and location implemented. Callback server in works

--- lib/group_me_bot/bot.rb
+++ lib/group_me_bot/bot.rb
@@ -1,22 +1,79 @@
-require 'net/http'
+require "json"
+require "net/http"
+require "socket"
+require "uri"
 
 module GroupMeBot
   class Bot
-    attr_accessor :bot_id, :post_uri
+    attr_accessor :bot_id, :callback_port, :post_uri
     POST_URI = "https://api.groupme.com/v3/bots/post"
     
-    def initialize(bot_id=nil, post_uri=POST_URI)
-      @bot_id   = bot_id
-      @post_uri = URI.parse(post_uri)
+    def initialize(bot_id = nil, callback_port = 8080, post_uri = POST_URI)
+      @bot_id        = bot_id
+      @callback_port = callback_port
+      @post_uri      = URI.parse(post_uri)
     end
 
     def send_message(message)
-      payload = {
-                  'bot_id' => self.bot_id,
-                  'text'   => message
-                }
-      
-      response = Net::HTTP.post_form(self.post_uri, payload)
+      req = Net::HTTP::Post.new(self.post_uri, initheader = { "Content-Type" => "application/json" })
+
+      req.body = {
+                  "bot_id" => self.bot_id,
+                  "text"   => message
+                 }.to_json
+
+      res = Net::HTTP.start(self.post_uri.host, self.post_uri.port, :use_ssl => self.post_uri.scheme == "https") do |http|
+        http.request(req)
+      end
+    end
+
+    def send_image(url)
+      req = Net::HTTP::Post.new(self.post_uri, initheader = { "Content-Type" => "application/json" })
+
+      req.body = {
+                  "bot_id"      => self.bot_id,
+                  "attachments" => [{
+                                     "type" => "image",
+                                     "url"  => url
+                                   }]
+                 }.to_json
+
+      res = Net::HTTP.start(self.post_uri.host, self.post_uri.port, :use_ssl => self.post_uri.scheme == "https") do |http|
+        http.request(req)
+      end
+    end
+
+    def send_location(long, lat, name)
+      req = Net::HTTP::Post.new(self.post_uri, initheader = { "Content-Type" => "application/json" })
+
+      req.body = {
+                  "bot_id"      => self.bot_id,
+                  "attachments" => [{
+                                     "type" => "location",
+                                     "lng"  => long.to_s,
+                                     "lat"  => lat.to_s,
+                                     "name" => name
+                                   }]
+                 }.to_json
+
+      res = Net::HTTP.start(self.post_uri.host, self.post_uri.port, :use_ssl => self.post_uri.scheme == "https") do |http|
+        http.request(req)
+      end
+    end
+
+    def run_callback_server(&callback_block)
+      Thread.start { callback_server(callback_block) }
+      puts "Callback server running on port #{self.callback_port}"
+    end
+
+    private
+    def callback_server(&callback_block)
+      server = TCPServer.new '127.0.0.1', self.callback_port
+      loop do
+        Thread.start(server.accept) do |client|
+          
+        end
+      end
     end
   end
 end
